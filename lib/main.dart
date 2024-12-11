7da7ec06bbc540feb44062b680376c1b70da6787
ashmita <ashmita0shahi@gmail.com>
2024-12-11 22:10:52 +0545

    screen file

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
-//started mob app
+import 'package:bookit/app.dart';
+import 'package:bookit/view/dashboard.dart';
+import 'package:bookit/view/login_page.dart';
+import 'package:bookit/view/register_page.dart';
+import 'package:bookit/view/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      initialRoute: "/",
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => LoginPage(),
+        "/register": (context) => RegisterPage(),
+        "/dashboard": (context) => Dashboard(),
+      }));
+}
