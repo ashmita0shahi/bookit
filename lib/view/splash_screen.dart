f1a18275948c97fc1812c4b24644fb59c178d269
ashmita <ashmita0shahi@gmail.com>
2024-12-12 07:46:37 +0545

    splash screen done

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bookit/view/login_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -8,11 +9,38 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  void _navigateToLogin() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text("This is splash screen"),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icons/logo.png',
+              width: 300,
+              height: 150,
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(
+              color: Color.fromARGB(255, 17, 47, 74),
+            ),
+          ],
+        ),
       ),
     );
   }
