8cf9b810d84e5ae6ac6a1e7b6380f16ab8a54492
ashmita <ashmita0shahi@gmail.com>
2024-12-20 13:41:40 +0545

    app theme added

--- lib/core/app_theme/app_theme.dart
+++ lib/core/app_theme/app_theme.dart
@@ -2,30 +2,43 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      primarySwatch: Colors.blue,
-      scaffoldBackgroundColor: const Color(0xffEFF6FF),
-      fontFamily: 'Montserrat Bold',
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-            textStyle: const TextStyle(
-              fontSize: 20,
-              color: Color.fromARGB(255, 255, 255, 255),
-              fontWeight: FontWeight.w500,
-              fontFamily: 'Montserrat Bold',
-            ),
-            backgroundColor: const Color(0xff003D93),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25),
-            )),
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: const Color(0xffEFF6FF),
+    fontFamily: 'Montserrat',
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      iconTheme: IconThemeData(
+        color: Colors.black,
       ),
-      appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          color: Color.fromARGB(255, 255, 255, 255),
-          elevation: 4,
-          shadowColor: Colors.black,
-          titleTextStyle: TextStyle(
-            fontSize: 16,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          )));
+      actionsIconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+      titleTextStyle: TextStyle(
+        fontSize: 20,
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+        backgroundColor: const Color(0xff003D93),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    ),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(
+        fontSize: 14,
+        color: Colors.black87,
+        fontWeight: FontWeight.w400,
+      ),
+    ),
+  );
 }
