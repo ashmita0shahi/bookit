1f59364ae11ae0f78cc1e10f0dac8cabf6a30682
ashmita <ashmita0shahi@gmail.com>
2025-01-31 17:47:25 +0545

    repository

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,14 +1,18 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/error/failure.dart';
 import '../entity/auth_entity.dart';
 
 abstract interface class IAuthRepository {
-  Future<Either<Failure, void>> registerStudent(AuthEntity student);
+  Future<Either<Failure, void>> registerUser(AuthEntity user);
 
-  Future<Either<Failure, String>> loginStudent(String email, String password);
+  Future<Either<Failure, String>> loginUser(String email, String password);
 
-  // Future<Either<Failure, String>> uploadProfilePicture(File file);
+  Future<Either<Failure, String>> uploadProfilePicture(File file);
+
+  Future<Either<Failure, void>> verifyEmail(String email, String otp);
 
   Future<Either<Failure, AuthEntity>> getCurrentUser();
 }
