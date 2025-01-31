66667ca767c8bbc60ab8d67bfca4c97c0306b41a
ashmita <ashmita0shahi@gmail.com>
2025-01-31 17:52:55 +0545

    updated DI from local to remote

--- lib/app/di/di.dart
+++ lib/app/di/di.dart
@@ -1,62 +1,112 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../core/network/api_service.dart';
 import '../../core/network/hive_service.dart';
 import '../../features/auth/data/data_source/local_data_source/auth_local_data_source.dart';
+import '../../features/auth/data/data_source/remote_data_source/auth_remote_data_source.dart';
 import '../../features/auth/data/repository/local_repository/auth_local_repository.dart';
+import '../../features/auth/data/repository/remote_repository/auth_remote_repository.dart';
 import '../../features/auth/domain/use_case/login_usecase.dart';
 import '../../features/auth/domain/use_case/register_usecase.dart';
+import '../../features/auth/domain/use_case/upload_image_usecase.dart';
+import '../../features/auth/domain/use_case/verify_usecase.dart';
 import '../../features/auth/presentation/view_model/login/login_bloc.dart';
 import '../../features/auth/presentation/view_model/register/register_bloc.dart';
 import '../../features/home/presentation/view_model/home_cubit.dart';
+import '../shared_prefs/token_shared_prefs.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> initDependencies() async {
-  // Initialize Hive Service
   _initHiveService();
-
-  // Initialize Auth Dependencies
-  _initAuthDependencies();
-
-  // Initialize Home Dependencies
+  _initApiService();
+  await _initSharedPreferences();
+  await _initLoginDependencies();
+  await _initSignupDependencies();
   _initHomeDependencies();
+}
+
+Future<void> _initSharedPreferences() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+}
+
+void _initApiService() {
+  getIt.registerLazySingleton<Dio>(
+    () => ApiService(Dio()).dio,
+  );
 }
 
 void _initHiveService() {
   getIt.registerLazySingleton<HiveService>(() => HiveService());
 }
 
-void _initAuthDependencies() {
-  // Auth Local Data Source
+void _initHomeDependencies() {
+  getIt.registerFactory<HomeCubit>(
+    () => HomeCubit(),
+  );
+}
+
+// **FIXED: Added missing VerifyEmailUsecase registration**
+Future<void> _initSignupDependencies() async {
   getIt.registerLazySingleton<AuthLocalDataSource>(
-      () => AuthLocalDataSource(getIt<HiveService>()));
-
-  // Auth Repository
-  getIt.registerLazySingleton<AuthLocalRepository>(
-      () => AuthLocalRepository(getIt<AuthLocalDataSource>()));
-
-  // Use Cases
-  getIt.registerLazySingleton<LoginUseCase>(
-      () => LoginUseCase(getIt<AuthLocalRepository>()));
-  getIt.registerLazySingleton<RegisterUseCase>(
-      () => RegisterUseCase(getIt<AuthLocalRepository>()));
-
-  // Login Bloc
-  getIt.registerFactory<LoginBloc>(
-    () => LoginBloc(
-      loginUseCase: getIt<LoginUseCase>(),
-      homeCubit: getIt<HomeCubit>(),
-    ),
+    () => AuthLocalDataSource(getIt<HiveService>()),
   );
 
-  // Register Bloc
+  getIt.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSource(getIt<Dio>()),
+  );
+
+  getIt.registerLazySingleton<AuthLocalRepository>(
+    () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
+  );
+
+  getIt.registerLazySingleton<AuthRemoteRepository>(
+    () => AuthRemoteRepository(getIt<AuthRemoteDataSource>()),
+  );
+
+  getIt.registerLazySingleton<RegisterUseCase>(
+    () => RegisterUseCase(getIt<AuthRemoteRepository>()),
+  );
+
+  getIt.registerLazySingleton<UploadImageUsecase>(
+    () => UploadImageUsecase(getIt<AuthRemoteRepository>()),
+  );
+
+  // **FIXED: Register VerifyEmailUsecase before using it**
+  getIt.registerLazySingleton<VerifyEmailUsecase>(
+    () => VerifyEmailUsecase(getIt<AuthRemoteRepository>()),
+  );
+
   getIt.registerFactory<RegisterBloc>(
     () => RegisterBloc(
       registerUseCase: getIt<RegisterUseCase>(),
+      verifyEmailUsecase: getIt<VerifyEmailUsecase>(),
+      // uploadImageUsecase:
+      // getIt<UploadImageUsecase>(), // Fixed: Added UploadImageUsecase
     ),
   );
 }
 
-void _initHomeDependencies() {
-  getIt.registerFactory<HomeCubit>(() => HomeCubit());
+// **FIXED: Ensure TokenSharedPrefs is injected in LoginUseCase**
+Future<void> _initLoginDependencies() async {
+  getIt.registerLazySingleton<TokenSharedPrefs>(
+    () => TokenSharedPrefs(getIt<SharedPreferences>()),
+  );
+
+  getIt.registerLazySingleton<LoginUseCase>(
+    () => LoginUseCase(
+      getIt<AuthRemoteRepository>(),
+      // getIt<TokenSharedPrefs>(), // Fixed: Injected TokenSharedPrefs
+    ),
+  );
+
+  getIt.registerFactory<LoginBloc>(
+    () => LoginBloc(
+      homeCubit: getIt<HomeCubit>(),
+      loginUseCase: getIt<LoginUseCase>(),
+    ),
+  );
 }
