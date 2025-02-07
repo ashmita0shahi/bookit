34d81e5df50948d52505e550ee588e1a208432b5
ashmita <ashmita0shahi@gmail.com>
2025-02-07 20:12:33 +0545

    state changed

--- lib/features/auth/presentation/view_model/login/login_state.dart
+++ lib/features/auth/presentation/view_model/login/login_state.dart
@@ -1,6 +1,33 @@
 part of 'login_bloc.dart';
 
-class LoginState {
+// class LoginState {
+//   final bool isLoading;
+//   final bool isSuccess;
+
+//   const LoginState({
+//     required this.isLoading,
+//     required this.isSuccess,
+//   });
+
+//   factory LoginState.initial() {
+//     return const LoginState(
+//       isLoading: false,
+//       isSuccess: false,
+//     );
+//   }
+
+//   LoginState copyWith({
+//     bool? isLoading,
+//     bool? isSuccess,
+//   }) {
+//     return LoginState(
+//       isLoading: isLoading ?? this.isLoading,
+//       isSuccess: isSuccess ?? this.isSuccess,
+//     );
+//   }
+// }
+
+class LoginState extends Equatable {
   final bool isLoading;
   final bool isSuccess;
 
@@ -10,19 +37,16 @@ class LoginState {
   });
 
   factory LoginState.initial() {
-    return const LoginState(
-      isLoading: false,
-      isSuccess: false,
-    );
+    return const LoginState(isLoading: false, isSuccess: false);
   }
 
-  LoginState copyWith({
-    bool? isLoading,
-    bool? isSuccess,
-  }) {
+  LoginState copyWith({bool? isLoading, bool? isSuccess}) {
     return LoginState(
       isLoading: isLoading ?? this.isLoading,
       isSuccess: isSuccess ?? this.isSuccess,
     );
   }
+
+  @override
+  List<Object> get props => [isLoading, isSuccess]; // Ensure proper comparison
 }
